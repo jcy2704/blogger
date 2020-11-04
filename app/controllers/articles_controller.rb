497e9b2b47f5aace57395b2582b5e4d9b5441815
Steven Jack Chung <stevenjchung12@gmail.com>
2020-11-04 14:38:44 -0500

    Add edit and update methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,17 @@ class ArticlesController < ApplicationController
     @article = Article.destroy(params[:id])
     @article.save
 
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
     redirect_to article_path(@article)
   end
 end
