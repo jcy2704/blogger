c08069d63aeaed7567d5b5e0b1b31625d02eccde
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2020-11-04 21:53:28 +0200

    Add flash notices to destroy, create and update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,14 +16,14 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-
+    flash.notice = "Article '#{@article.title}' created"
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.destroy(params[:id])
     @article.save
-
+    flash.notice = "Article '#{@article.title}' Deleted!"
     redirect_to articles_path
   end
 
@@ -34,7 +34,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
-
+    flash.notice = "Article '#{@article.title}' Updated"
     redirect_to article_path(@article)
   end
 end
